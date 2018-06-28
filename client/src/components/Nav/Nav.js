67c3bb5ea44c2da8e197e86fc38274c361b0496d
Matt <mjosephjefferis@gmail.com>
2018-06-27 21:12:43 -0400

    improved navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,24 +1,25 @@
 import React from "react";
 import "./Nav.css";
 
+const Background = 'darkness.png';
+const style = {
+  backgroundImage: `url(${Background})`
+}
+
 const Nav = () => (
 
-
-
-<nav className="navbar">
+<nav style={style} className="navbar">
   <div className="container-fluid">
     <div className="navbar-header">
       <a className="navbar-brand" href="/">BandBase</a>
     </div>
-
     <ul className="nav navbar-nav navbar-right">
       <li><a href="#"><span className ="glyphicon glyphicon-user"></span> Sign Up</a></li>
       <li><a href="#"><span className ="glyphicon glyphicon-log-in"></span> Login</a></li>
     </ul>
   </div>
 </nav>
-
-
 );
+
 
 export default Nav;
