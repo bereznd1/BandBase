2c51232df1ad3f6b61aa3f1add8b00bea4bdc8c6
Isaac <isaaclubanko@gmail.com>
2018-06-30 13:23:30 -0400

    added passport foncig

--- routes/api/bands.js
+++ routes/api/bands.js
@@ -11,8 +11,27 @@ var session      = require('express-session');
 
 //-----------------PASSPORT VALIDATION--------------------------//
 
-//-----------------PASSPORT CONFIGUREATION
 
+//-----------------PASSPORT CONFIGURATION----------------------//
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+//-----------------PASSPORT CONFIGURATION----------------------//
 
 // Matches with "/api/books"
 router.route("/")
