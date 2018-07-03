9c20da57b5db038a548368bf1332dcbda771a26c
Isaac <isaaclubanko@gmail.com>
2018-07-02 21:29:17 -0400

    post signups work

--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -1,11 +1,32 @@
 const router = require("express").Router();
-const bandsController = require("../../controllers/bandsController");
+// const bandsController = require("../../controllers/bandsController");
+const User = require('../../models/band')
 
-router.get('/', (req,res) => {
-    console.log('signup is working')
-    res.json(200);
+// router.get('/', (req,res) => {
+//     console.log('signup is working')
+//     res.json(200);
+// })
+
+
+router.post('/', (req, res) => {
+	const { username, password } = req.body
+	// ADD VALIDATION
+	User.findOne({ 'username': username }, (err, userMatch) => {
+		if (userMatch) {
+			return res.json({
+				error: `Sorry, already a user with the username: ${username}`
+			})
+		}
+		const newUser = new User({
+			'username': username,
+			'password': password
+		})
+		newUser.save((err, savedUser) => {
+			if (err) return res.json(err)
+			return res.json(savedUser)
+		})
+	})
 })
-
 
 
 
