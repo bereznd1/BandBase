f368b9951b7ee88d527540d3c60fe4c56d8995e5
Isaac <isaaclubanko@gmail.com>
2018-07-05 11:20:09 -0400

    logout route now added

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
-const loginRoutes = require("./login");
 const signupRoutes = require("./signup");
+const loginRoutes = require("./login");
+const logoutRoutes = require("./logout");
 
 // Book routes
-router.use("/login", loginRoutes);
 router.use("/signup", signupRoutes);
+router.use("/login", loginRoutes);
+router.use("/logout", logoutRoutes);
+
+
 
 module.exports = router;
