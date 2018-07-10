b88c9c1d235eaab38088b83017173940138000dd
Isaac <isaaclubanko@gmail.com>
2018-07-10 13:57:40 -0400

    app is now a class

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Bands from "./pages/Bands";
 // import Login from "./pages/Login";
@@ -6,21 +6,43 @@ import AllBands from "./pages/AllBands/AllBands";
 import Profile from "./pages/Profile";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			loggedIn: false,
+			user: null
+		}
+		this._logout = this._logout.bind(this)
+    this._login = this._login.bind(this)
+  }
 
-const App = () => (
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Bands} />
-        {/* <Route exact path="/login" component={Login} /> */}
-        <Route exact path="/bands" component={Bands} />
-        <Route exact path="/allbands" component={AllBands} />
-        <Route exact path="/api/bands/:id" component={Profile} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>
-);
+  _logout(event) {
+		event.preventDefault()
+		console.log('logging out')
+	}
+
+	_login(username, password) {
+    console.log(username + password);
+	};
+  
+  render() {
+		return (
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Bands} />
+            {/* <Route exact path="/login" component={Login} /> */}
+            <Route exact path="/bands" component={Bands} />
+            <Route exact path="/allbands" component={AllBands} />
+            <Route exact path="/api/bands/:id" component={Profile} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+      );
+    }
+}
 
 export default App;
