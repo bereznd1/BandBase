625395bad94ae4adc41159657c28db9acaea7c91
Matt <mjosephjefferis@gmail.com>
2018-07-11 20:42:07 -0400

    divs have actual margins and shadows

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Footer.css";
 
+const Background = "darkness.png";
+const style = {
+  backgroundImage: `url(${Background})`
+};
+
 const Footer = () => (
-  <div className="footer">
+  <div className="footer" style={style}>
     <h1 className="text-primary">&copy; BandBase</h1>
   </div>
 );
