6f0dfd2c6224c62de9b73bd1bd1a96ae8013876e
Isaac <isaaclubanko@gmail.com>
2018-07-14 10:32:56 -0400

    now updates correctly

--- routes/auth/update.js
+++ routes/auth/update.js
@@ -4,12 +4,20 @@ const express = require('express')
 const router = require("express").Router();
 const User = require('../../models/band')
 const passport = require('../../passport')
+const db = require("../../models");
 
 
 router.put('/:id', (req,res) => {
     console.log("yeah");
     if (req.user) {
-        bandsController.update()
+        console.log(req.user);
+        db.Band
+        .findOneAndUpdate({ _id: req.params.id }, req.body, {new: true})
+        .then(dbModel => 
+            {
+                return res.json(dbModel);
+            })
+        .catch(err => res.status(422).json(err));
     } else {
         res.status(401).json({});
         console.log("it does not work");
